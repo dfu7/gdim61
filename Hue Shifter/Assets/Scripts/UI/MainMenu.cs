80b4a00780c11f8b0e8e4311526a4069a9119fab
Benjamin Wong <chiyeonwastaken@gmail.com>
2022-04-19 00:46:24 -0700

    added audio mixers & minimal main menu

---
+++ Hue Shifter/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+   [SerializeField] private AudioMixer _mixer;
+   [SerializeField] private GameObject[] pages;
+
+   /* Change scene based on build index. Use for UI buttons. */
+   public void SetScene(int buildIndex) {
+      SceneManager.LoadScene(buildIndex);
+   }
+
+   /* load a 'page.' page index can be found in the name of the page gameobject */
+   public void SetPage(int pageIndex) {
+      if(pages[pageIndex] != null) {
+         for(int i = 0; i < pages.Length; i++) {
+            pages[i].SetActive(pageIndex == i ? true : false);
+         }
+      }
+   }
+   
+   /* quits application */
+   public void QuitGame() {
+      Application.Quit(0);
+   }
+
+   /* The following 3 are for volume sliders, linked to the master _mixer */
+   public void SetMasterVolume(float sliderValue) {
+      _mixer.SetFloat("MasterVolume", Mathf.Log10(sliderValue) * 20);
+   }
+
+   public void SetSoundVolume(float sliderValue) {
+      _mixer.SetFloat("SoundVolume", Mathf.Log10(sliderValue) * 20);
+   }
+
+   public void SetMusicVolume(float sliderValue) {
+      _mixer.SetFloat("MusicVolume", Mathf.Log10(sliderValue) * 20);
+   }
+}
