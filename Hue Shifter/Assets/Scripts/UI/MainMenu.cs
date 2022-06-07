09f050e550fa657f0f024d192e4a33706ac6d0aa
Benjamin Wong <44148480+chiyeon@users.noreply.github.com>
2022-06-07 07:28:59 -0700

    added UI click sounds

--- Hue Shifter/Assets/Scripts/UI/MainMenu.cs
+++ Hue Shifter/Assets/Scripts/UI/MainMenu.cs
@@ -24,11 +24,15 @@ public class MainMenu : MonoBehaviour
 
     /* Change scene based on build index. Use for UI buttons. */
     public void SetScene(int buildIndex) {
+       // play sound
+      SoundManager.instance.Play("Click");
       SceneManager.LoadScene(buildIndex);
    }
 
    /* load a 'page.' page index can be found in the name of the page gameobject */
    public void SetPage(int pageIndex) {
+      // play sound
+      SoundManager.instance.Play("Click");
       if(pages[pageIndex] != null) {
          for(int i = 0; i < pages.Length; i++) {
             pages[i].SetActive(pageIndex == i ? true : false);
@@ -38,19 +42,27 @@ public class MainMenu : MonoBehaviour
    
    /* quits application */
    public void QuitGame() {
+      // play sound
+      SoundManager.instance.Play("Click");
       Application.Quit(0);
    }
 
    /* The following 3 are for volume sliders, linked to the master _mixer */
    public void SetMasterVolume(float sliderValue) {
+      // play sound
+      SoundManager.instance.Play("Click");
       _mixer.SetFloat("MasterVolume", Mathf.Log10(sliderValue) * 20);
    }
 
    public void SetSoundVolume(float sliderValue) {
+      // play sound
+      SoundManager.instance.Play("Click");
       _mixer.SetFloat("SoundVolume", Mathf.Log10(sliderValue) * 20);
    }
 
    public void SetMusicVolume(float sliderValue) {
+      // play sound
+      SoundManager.instance.Play("Click");
       _mixer.SetFloat("MusicVolume", Mathf.Log10(sliderValue) * 20);
    }
 }
